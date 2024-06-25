95a3b834e5b16da2a5311da5adf79e5643408e95
Upeka <chamudiupeka20@gmail.com>
2024-06-25 06:37:38 +0530

    update the 4th code

--- src/Forth.scala
+++ src/Forth.scala
@@ -1,11 +1,24 @@
+import scala.collection.generic.BitOperations
+
 object Forth {
-  def main(args: Array[String]): Unit={
-    val basePrice = 15.0
+  def main(args: Array[String]): Unit = {
+    val basePrice = 15
     val baseAttendance = 120
-    val priceStep = 5.0
+    val priceStep = 5
     val attendanceChange = 20
-    val fixedCost = 500.0
-    val variableCostPerAttendee = 3.0
+    val fixedCost = 500
+
+
+    def attendance(ticketPrice: Int): Int = baseAttendance + (basePrice - ticketPrice) / priceStep * attendanceChange
+
+    def revenue(ticketPrice: Int): Int = ticketPrice * attendance(ticketPrice)
+
+    def cost(ticketPrice: Int): Int = fixedCost + attendance(ticketPrice)
+
+    def profit(ticketPrice: Int): Int = revenue(ticketPrice) - cost(ticketPrice)
+
+    print(profit(5), profit(10), profit(15), profit(20))
+
 
   }
 
